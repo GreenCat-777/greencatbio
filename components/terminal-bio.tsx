c50b045c9fae44e0234fc70b2ec67a2430d17c38
GreenCat777 <greencat777456@gmail.com>
2026-01-05 11:39:49 -0600

    Update terminal-bio.tsx

--- components/terminal-bio.tsx
+++ components/terminal-bio.tsx
@@ -17,7 +17,7 @@ I'm a beta tester for the MIP wiki and KindleForge. I've written pages for the M
 
 All my projects (not my documentation) are listed under The Unlicense, with no rights reserved, If you use my work credit is appreiciated, but NOT required. If you have paid for any of my work you have been legally scammed.
 
-If you need to reach out to me, or think I could be a valuable collaborator on your project, contact me from the Email or Discord buttons below (Discord Preferred). If you want to take a look at my work use the Github button and/or look in the KMC. I have also started a wiki which has documentation on my projects.`
+If you need to reach out to me, or think I could be a valuable collaborator on your project, contact me from the Email or Discord buttons below (Discord Preferred). If you want to take a look at my work use the Github button and/or look in the KMC. I also have a wiki with proper documentation on my work.`
 
   useEffect(() => {
     const introTimer = setTimeout(() => {
