c0d2f1bcde867495d10537e6aa4dfb7ffb0c5f26
GreenCat777 <greencat777456@gmail.com>
2026-05-10 01:38:35 -0500

    Add files via upload

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 export function createServerSupabase() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.STORAGE_SUPABASE_SECRET_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE!,
     { auth: { persistSession: false } }
   );
 }
