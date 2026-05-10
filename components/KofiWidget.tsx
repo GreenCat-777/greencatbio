ca47d0e129cf98a1da517e4f0958b6f51a9037ad
GreenCat777 <greencat777456@gmail.com>
2026-05-10 15:10:11 -0500

    Update KofiWidget.tsx

--- components/KofiWidget.tsx
+++ components/KofiWidget.tsx
@@ -19,8 +19,8 @@ export default function KofiWidget() {
       window.kofiWidgetOverlay.draw("gc777", {
         type: "floating-chat",
         "floating-chat.donateButton.text": "Support me",
-        "floating-chat.donateButton.background-color": "#00b9fe",
-        "floating-chat.donateButton.text-color": "#fff",
+        "floating-chat.donateButton.background-color": "#00000",
+        "floating-chat.donateButton.text-color": "#0fe54b",
       });
     };
     document.body.appendChild(script);
